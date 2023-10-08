dd894ac982bb2a8e7ab67ae165f471764a6cd979
Suryavamsi53 <63606535+Suryavamsi53@users.noreply.github.com>
2023-10-09 00:36:16 +0530

    Update JobSeekerOperations.cs

--- ConsultancyBusinessLogicLayer/JobSeekerOperations.cs
+++ ConsultancyBusinessLogicLayer/JobSeekerOperations.cs
@@ -6,7 +6,7 @@ namespace ConsultancyBusinessLogicLayer
 {
     public class JobSeekerOperation
     {
-        // ... Other existing code ...
+
 
         public class Jobs
         {
@@ -14,18 +14,17 @@ namespace ConsultancyBusinessLogicLayer
 
             public void PostRequirement(Employer employer, JobRequirement requirement)
             {
-                // You can associate the requirement with the employer
+             
                 requirement.EmployerId = employer.Id;
                 jobRequirements.Add(requirement);
             }
 
             public void EditRequirement(Employer employer, int requirementId, JobRequirement updatedRequirement)
             {
-                // Find the requirement by ID and ensure it belongs to the employer
+                
                 var requirement = jobRequirements.FirstOrDefault(r => r.Id == requirementId && r.EmployerId == employer.Id);
                 if (requirement != null)
                 {
-                    // Update the requirement details
                     requirement.Title = updatedRequirement.Title;
                     requirement.Description = updatedRequirement.Description;
                     requirement.SkillsRequired = updatedRequirement.SkillsRequired;
@@ -38,7 +37,8 @@ namespace ConsultancyBusinessLogicLayer
 
             public void DeleteRequirement(Employer employer, int requirementId)
             {
-                // Find the requirement by ID and ensure it belongs to the employer
+
+                
                 var requirement = jobRequirements.FirstOrDefault(r => r.Id == requirementId && r.EmployerId == employer.Id);
                 if (requirement != null)
                 {
@@ -52,7 +52,7 @@ namespace ConsultancyBusinessLogicLayer
 
             public List<JobRequirement> SearchJobs(string keyword)
             {
-                // Search for job requirements that match the keyword in title or description
+               
                 return jobRequirements
     .Where(r => r.Title.IndexOf(keyword, StringComparison.OrdinalIgnoreCase) >= 0 ||
                 r.Description.IndexOf(keyword, StringComparison.OrdinalIgnoreCase) >= 0)
@@ -72,14 +72,13 @@ namespace ConsultancyBusinessLogicLayer
 
         public class Employer
         {
-            public int Id { get; set; } // Add an identifier for the employer
+            public int Id { get; set; } 
             public string CompanyName { get; set; }
             public string ContactEmail { get; set; }
             public string Industry { get; set; }
         }
 
-        // ... Other existing code ...
+ 
     }
 
-    // ... Other existing code ...
 }
